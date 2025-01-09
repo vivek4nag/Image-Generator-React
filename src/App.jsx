cc71377e5871ca7728d55ab0d4b49912d0e0ddda
viek4nag <vivek4nag@gmail.com>
2025-01-09 23:17:22 +0530

    code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import ImageContainer from "./components/ImageContainer"
 
 
 function App() {
   return (
     <div className="w-full h-screen bg-slate-900 text-white">
       <h1>image generator</h1>
+      <ImageContainer/>
     </div>
   )
 }
